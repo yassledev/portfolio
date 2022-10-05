5a32f69b4f8b8deeb1eda591e5aa944d399a8028
Yass <yassledev@outlook.com>
2022-10-05 19:23:49 +0200

    ✨ add push-dir to create github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    typescript: {
+        shim: false
+    },
+    target: 'static',
+    ssr: false,
+    router: {
+        base: '/portfolio/'
+    }
 })
