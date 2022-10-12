9550ebc693e5bda6dca2a8a0348badc697736a19
Yass <yassledev@outlook.com>
2022-10-12 07:13:01 +0200

    fix: issue with github action "static html exoprt with nuxt"

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,7 @@
 <script lang="ts">
-    import Navbar from '@/components/Navbar.vue';
-    import FreelanceMessage from '@/components/FreelanceMessage.vue';
-
 </script>
 <template>
     <div>
-        <Navbar />
         <slot />
     </div>
 </template>
